5ede393a33a4a3110bac07f8a00228e9aeeba0fc
ynsgndz <yns3gndz@hotmail.com.tr>
2023-03-31 18:07:24 +0300

    item panel added

---
+++ wildbite/wildbite/Controller/ADMIN/ITEM/ItemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ItemViewController.swift
+//  wildbite
+//
+//  Created by Yunus Gunduz on 31.03.2023.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+ 
+
+}
