829d2f8bfe507273c5a32d0e772610888ee3558d
ynsgndz <yns3gndz@hotmail.com.tr>
2023-04-10 03:11:38 +0300

    register pagesi mantigi tanimlandi , kayit ol ile  user bilgileri aliniyor karakter secim ekranina yonlendirilip, karakter secim ekraninda irkini sectiriyoruz.

--- wildbite/wildbite/PAPER/PaperViewController.swift
+++ wildbite/wildbite/PAPER/PaperViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 import PaperOnboarding
 
-class PaperViewController: UIViewController,PaperOnboardingDataSource {
+class PaperViewController: UIViewController,PaperOnboardingDataSource,PaperOnboardingDelegate {
+   var secilenKarakter = 0
     func onboardingItem(at index: Int) -> OnboardingItemInfo {
 
         let titleFont = UIFont(name: "Nunito-Bold", size: 36.0) ?? UIFont.boldSystemFont(ofSize: 36.0)
@@ -16,45 +17,88 @@ class PaperViewController: UIViewController,PaperOnboardingDataSource {
         
        return [
         OnboardingItemInfo(informationImage: UIImage(named: "homeLight")!,
-                                       title: "title",
-                                 description: "description",
+                                       title: "HUNTER",
+                                 description: "He is the person who hunted wolves and vampires ",
                                     pageIcon: UIImage(named: "homeLight")!,
                          //  color: UIColor(red: 0.40, green: 0.56, blue: 0.71, alpha: 1.00),
                            color: UIColor.black,
-                                                    titleColor: UIColor.systemRed, descriptionColor: UIColor.systemRed, titleFont: titleFont, descriptionFont: descriptionFont),
+                                                    titleColor: UIColor.systemRed, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
         
         
          OnboardingItemInfo(informationImage: UIImage(named: "profileLight")!,
-                                        title: "title",
-                                  description: "description",
+                                        title: "WEREWOLF",
+                                  description: "Human Wolves of Wild Nature",
                                      pageIcon: UIImage(named: "profileLight")!,
                             //color: UIColor(red: 0.40, green: 0.69, blue: 0.71, alpha: 1.00),
-                            color: UIColor.systemRed,
-                                                      titleColor: UIColor.black, descriptionColor: UIColor.black, titleFont: titleFont, descriptionFont: descriptionFont),
+                            color: UIColor.black,
+                                                      titleColor: UIColor.systemRed, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
                                    
 
         OnboardingItemInfo(informationImage: UIImage(named: "settingsLight")!,
-                                     title: "title",
-                               description: "description",
+                                     title: "VAMPIRE",
+                               description: "Creature for blood",
                                   pageIcon: UIImage(named: "settingsLight")!,
                            // color: UIColor(red: 0.61, green: 0.56, blue: 0.74, alpha: 1.00),
                            color: UIColor.black,
-                                                     titleColor: UIColor.systemRed, descriptionColor: UIColor.systemRed, titleFont: titleFont, descriptionFont: descriptionFont)
+                                                     titleColor: UIColor.systemRed, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
          ][index]
      }
 
      func onboardingItemsCount() -> Int {
         return 3
       }
+    func onboardingDidTransitonToIndex(_ index: Int) {
+        print("Debug: didTransition")
+    }
+    func onboardingConfigurationItem(_ item: OnboardingContentViewItem, index: Int) {
+        print("Debug: Configuration")
+    }
+    func onboardingWillTransitonToLeaving() {
+        print("Debug: leaving")
+    }
+    
+    func onboardingWillTransitonToIndex(_ index: Int) {
+        if(index == 0){
+            UIView.animate(withDuration: 0.1) { [self] in
+                print("Debug: Index 0")
+                secilenKarakter = 0
+                print("Debug secilen karakter: \(secilenKarakter)")
+            }
+            
+        }
+        if(index == 1){
+            UIView.animate(withDuration: 0.9) { [self] in
+                print("Debug: Index 1")
+                secilenKarakter = 1
+                print("Debug secilen karakter: \(secilenKarakter)")
+            }
+        }
+            if(index == 2){
+                UIView.animate(withDuration: 0.9) { [self] in
+                    secilenKarakter = 2
+                    print("Debug secilen karakter: \(secilenKarakter)")
+                    print("Debug: Index 2")
+                }
+            }
+          
+    }
+    
+    
+    @IBAction func characterSelectButton(_ sender: Any) {
+    print("----- Secilen karakter \(secilenKarakter)")
+    }
     
     @IBOutlet weak var onboardingTwoView: myOnBoardign!
     override func viewDidLoad() {
+     
         super.viewDidLoad()
-
+        onboardingTwoView.delegate = self
         onboardingTwoView.dataSource = self
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("Debug secilen karakter: \(secilenKarakter)")
+    }
    
 
 }
