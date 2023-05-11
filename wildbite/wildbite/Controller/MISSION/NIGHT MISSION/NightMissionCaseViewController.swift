e037ac6736083de33a141ec7524671ac08c5cf30
ynsgndz <ynsgndz@hotmail.com.tr>
2023-05-11 21:51:18 +0300

    night mission eklendi apiyi ve ayarlarini bekliyorum

--- wildbite/wildbite/Controller/MISSION/NIGHT MISSION/NightMissionCaseViewController.swift
+++ wildbite/wildbite/Controller/MISSION/NIGHT MISSION/NightMissionCaseViewController.swift
@@ -6,29 +6,123 @@
 //
 
 import UIKit
+import Alamofire
+import Kingfisher
 
 class NightMissionCaseViewController: UIViewController {
 
     @IBOutlet weak var huntName: UILabel!
+    @IBOutlet weak var huntHealth: UILabel!
+    @IBOutlet weak var missionResult: UILabel!
     
+    @IBOutlet weak var attactButton: UIButton!
     @IBOutlet weak var huntPow: UILabel!
+    @IBOutlet weak var goldLabel: UILabel!
     
+    @IBOutlet weak var missionMonsterImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
 
-    override func viewWillAppear(_ animated: Bool) {
-       // let nightMissionType = UserDefaults.standard.value(forKey: "NightMissionType") as! Int
-        let nightHunterName = UserDefaults.standard.value(forKey: "NightHunterName")
-        let nightHunterPow = UserDefaults.standard.value(forKey: "NightHunterPow") as! Int
-     
-        huntName.text = "Name \(nightHunterName!)"
-        huntPow.text = "Power \(nightHunterPow)"
+    @IBAction func attackButton(_ sender: Any) {
+        let nightMissionType = UserDefaults.standard.integer(forKey: "NightMissionType")
         
+        let NightMissionUserLevel = UserDefaults.standard.integer(forKey: "NightMissionUserLevel")
+        let NightMissionUserName = UserDefaults.standard.string(forKey: "NightMissionUserName")
+        let NightMissionUserTotalDamage = UserDefaults.standard.integer(forKey: "NightMissionUserTotalDamage")
+        let NightMissionUserHealth = UserDefaults.standard.integer(forKey: "NightMissionUserHealth")
+        let NightMissionUserEnergy = UserDefaults.standard.integer(forKey: "NightMissionUserEnergy")
+        
+  
+        
+        
+            if(NightMissionUserTotalDamage >= nightMissionType * 10){
+               
+                missionResult.text = "\(NightMissionUserName!) WON \n\n + \(nightMissionType * 3)  Exp"
+               
+                attactButton.isHidden = true
+                missionResult.isHidden = false
+                goldLabel.isHidden = false
+            }else{
+                missionResult.isHidden = false
+                missionResult.text = "\(NightMissionUserName!) LOSE \n Health - \(nightMissionType * 10) \n Energy - \(nightMissionType * 5)"
+                missionResult.textColor = UIColor.red
+                
+                
+            }
+            
+            
+        
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        
+     
+        let nightMissionType = UserDefaults.standard.integer(forKey: "NightMissionType")
+        
+        let NightMissionUserLevel = UserDefaults.standard.integer(forKey: "NightMissionUserLevel")
+        let NightMissionUserName = UserDefaults.standard.string(forKey: "NightMissionUserName")
+        let NightMissionUserTotalDamage = UserDefaults.standard.integer(forKey: "NightMissionUserTotalDamage")
+        let NightMissionUserHealth = UserDefaults.standard.integer(forKey: "NightMissionUserHealth")
+        let NightMissionUserEnergy = UserDefaults.standard.integer(forKey: "NightMissionUserEnergy")
+        
+      
+     
+        let url = URL(string: "http://yunusgunduz.site/wildbite/image/missions/nightboss/\(nightMissionType).png")
+        DispatchQueue.main.async{ [self] in
+            missionMonsterImage.kf.setImage(with: url)
+        }
+        
+        huntPow.text = "Monster Power: \(nightMissionType * 10 )"
+        goldLabel.text = " +\(nightMissionType * 1000 ) GOLD"
+        huntHealth.text = "Health : \(nightMissionType * 100 ) "
+        
+        switch nightMissionType {
+            case 1:  huntName.text = "Shadowbeast"
+            case 3:  huntName.text = "Steelclaw"
+            case 5:  huntName.text = "Venomgaze"
+            case 7:  huntName.text = "Flamespirit"
+            case 9:  huntName.text = "Stoneroar"
+            case 10:  huntName.text = "Bloodsoil"
+            case 11:  huntName.text = "Shadowreaper"
+            case 13:  huntName.text = "Mindbreaker"
+            case 15:  huntName.text = "Windmyst"
+            case 17:  huntName.text = "Starcrusher"
+            case 19:  huntName.text = "Fearlessgaze"
+            case 20:  huntName.text = "Swordshatter"
+            case 21:  huntName.text = "Flamejudgment"
+            case 23:  huntName.text = "Venomclaw"
+            case 25:  huntName.text = "Ironrend"
+            case 27:  huntName.text = "Bloodhowl"
+            case 29:  huntName.text = "Shadowbane"
+            case 30:  huntName.text = "Echoessence"
+            case 31:  huntName.text = "Spiritwanderer"
+            case 33:  huntName.text = "Wingednightmare"
+            case 35:  huntName.text = "Darkwall"
+            case 37:  huntName.text = "Grandomenace"
+            case 39:  huntName.text = "Venomscout"
+            case 40:  huntName.text = "Stoneclaw"
+            case 41:  huntName.text = "Skyblade"
+            case 43:  huntName.text = "Bloodcleave"
+            case 45:  huntName.text = "Moonshadow"
+            case 47:  huntName.text = "Blackdeath"
+            case 49:  huntName.text = "Ironrend"
+            case 50:  huntName.text = "Thundergaze"
+               
+            default:  huntName.text = "monster name"
+        }
+   
       
     }
 
+    
+    
+    
+    
+    
+    
 }
--- wildbite/wildbite/Controller/MISSION/NIGHT MISSION/NightMissionViewController.swift
+++ wildbite/wildbite/Controller/MISSION/NIGHT MISSION/NightMissionViewController.swift
@@ -6,21 +6,41 @@
 //
 
 import UIKit
+import Alamofire
 
 class NightMissionViewController: UIViewController {
 
-    let userLevel = 10
     
-    
-    @IBOutlet weak var btnLvl1: UIButton!
-    @IBOutlet weak var btnLvl5: UIButton!
-    @IBOutlet weak var btnLvl10: UIButton!
-    @IBOutlet weak var btnLvl15: UIButton!
-    @IBOutlet weak var btnLvl20: UIButton!
-    @IBOutlet weak var btnLvl25: UIButton!
-    @IBOutlet weak var btnLvl30: UIButton!
-    @IBOutlet weak var btnLvl35: UIButton!
-   
+    @IBOutlet weak var nmButton1: UIButton!
+    @IBOutlet weak var nmButton2: UIButton!
+    @IBOutlet weak var nmButton3: UIButton!
+    @IBOutlet weak var nmButton4: UIButton!
+    @IBOutlet weak var nmButton5: UIButton!
+    @IBOutlet weak var nmButton6: UIButton!
+    @IBOutlet weak var nmButton7: UIButton!
+    @IBOutlet weak var nmButton8: UIButton!
+    @IBOutlet weak var nmButton9: UIButton!
+    @IBOutlet weak var nmButton10: UIButton!
+    @IBOutlet weak var nmButton11: UIButton!
+    @IBOutlet weak var nmButton12: UIButton!
+    @IBOutlet weak var nmButton13: UIButton!
+    @IBOutlet weak var nmButton14: UIButton!
+    @IBOutlet weak var nmButton15: UIButton!
+    @IBOutlet weak var nmButton16: UIButton!
+    @IBOutlet weak var nmButton17: UIButton!
+    @IBOutlet weak var nmButton18: UIButton!
+    @IBOutlet weak var nmButton19: UIButton!
+    @IBOutlet weak var nmButton20: UIButton!
+    @IBOutlet weak var nmButton21: UIButton!
+    @IBOutlet weak var nmButton22: UIButton!
+    @IBOutlet weak var nmButton23: UIButton!
+    @IBOutlet weak var nmButton24: UIButton!
+    @IBOutlet weak var nmButton25: UIButton!
+    @IBOutlet weak var nmButton26: UIButton!
+    @IBOutlet weak var nmButton27: UIButton!
+    @IBOutlet weak var nmButton28: UIButton!
+    @IBOutlet weak var nmButton29: UIButton!
+    @IBOutlet weak var nmButton30: UIButton!
     
     
     
@@ -30,68 +50,248 @@ class NightMissionViewController: UIViewController {
         super.viewDidLoad()
 
         
-       // btnLv1.titleLabel?.text  =  "asdasd"
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
       
     }
     
-    
-    
-    
-    @IBAction func btnLvl1Click(_ sender: Any) {
-        UserDefaults.standard.set(1, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter1", forKey: "NightHunterName")
-        UserDefaults.standard.set(10, forKey: "NightHunterPow")
+    override func viewWillAppear(_ animated: Bool) {
+        let myUserID = UserDefaults.standard.value(forKey: "userID")
+        let myUserName = UserDefaults.standard.value(forKey: "userName")
+        let myUserToken = UserDefaults.standard.value(forKey: "userToken")
+        print("User Id: \(myUserID!)")
+        print("User Name: \(myUserName!)")
+        print("User Token: \(myUserToken!)")
         
-        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+                let token = "\(myUserToken!)"
+                
+                let headers: HTTPHeaders = [
+                
+                    .authorization(bearerToken: token),
+                    .accept("application/json")
+                    
+                ]
         
-        
-    }
-    @IBAction func btnLvl5Click(_ sender: Any) {
-        UserDefaults.standard.set(5, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter2", forKey: "NightHunterName")
-        UserDefaults.standard.set(50, forKey: "NightHunterPow")
-        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
-    }
-    @IBAction func btnLvl10Click(_ sender: Any) {
-        UserDefaults.standard.set(10, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter3", forKey: "NightHunterName")
-        UserDefaults.standard.set(100, forKey: "NightHunterPow")
-        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
-    }
-    @IBAction func btnLvl15Click(_ sender: Any) {
-        UserDefaults.standard.set(15, forKey: "NightMissionType")
      
-        UserDefaults.standard.set("Hunter4", forKey: "NightHunterName")
-        UserDefaults.standard.set(150, forKey: "NightHunterPow")
+      
+        AF.request("http://yunusgunduz.site/wildbite/public/api/user/\(myUserID!))" , headers: headers )
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { [self] response in
+                
+                
+                switch response.result {
+                case .success:
+                    print("Validation Successful")
+                    
+                    let profileModelresponse = try? JSONDecoder().decode(ProfileModel.self, from: response.data!)
+                    debugPrint(profileModelresponse!)
+                        
+                        UserDefaults.standard.set(profileModelresponse!.user.name, forKey: "NightMissionUserName")
+                        UserDefaults.standard.set(profileModelresponse!.user.totalDamage, forKey: "NightMissionUserTotalDamage")
+                        UserDefaults.standard.set(profileModelresponse!.user.currentHealth, forKey: "NightMissionUserHealth")
+                        UserDefaults.standard.set(profileModelresponse!.user.currentEnergy, forKey: "NightMissionUserEnergy")
+                        UserDefaults.standard.set(profileModelresponse!.user.level, forKey: "NightMissionUserLevel")
+                        
+                        let missionYapilabilmedurumu =  "3" // apiden gelcek  ve ona gore duzenlenıcek bu kısım
+                        let userLevel = Int(profileModelresponse!.user.level)
+                        
+                        if( userLevel! <= 1  && missionYapilabilmedurumu == "1") {nmButton1.isEnabled = true}
+                        if( userLevel! <= 3  && missionYapilabilmedurumu == "3") {nmButton2.isEnabled = true}
+                        if( userLevel! <= 5  && missionYapilabilmedurumu == "5") {nmButton3.isEnabled = true}
+                        if(profileModelresponse!.user.level == "7") {nmButton4.isEnabled = true}
+                        if(profileModelresponse!.user.level == "9") {nmButton5.isEnabled = true}
+                        if(profileModelresponse!.user.level == "10") {nmButton6.isEnabled = true}
+                        if(profileModelresponse!.user.level == "11") {nmButton7.isEnabled = true}
+                        if(profileModelresponse!.user.level == "13") {nmButton8.isEnabled = true}
+                        if(profileModelresponse!.user.level == "15") {nmButton9.isEnabled = true}
+                        if(profileModelresponse!.user.level == "17") {nmButton10.isEnabled = true}
+                        if(profileModelresponse!.user.level == "19") {nmButton11.isEnabled = true}
+                        if(profileModelresponse!.user.level == "20") {nmButton12.isEnabled = true}
+                        if(profileModelresponse!.user.level == "21") {nmButton13.isEnabled = true}
+                        if(profileModelresponse!.user.level == "23") {nmButton14.isEnabled = true}
+                        if(profileModelresponse!.user.level == "25") {nmButton15.isEnabled = true}
+                        if(profileModelresponse!.user.level == "27") {nmButton16.isEnabled = true}
+                        if(profileModelresponse!.user.level == "29") {nmButton17.isEnabled = true}
+                        if(profileModelresponse!.user.level == "30") {nmButton18.isEnabled = true}
+                        if(profileModelresponse!.user.level == "31") {nmButton19.isEnabled = true}
+                        if(profileModelresponse!.user.level == "33") {nmButton20.isEnabled = true}
+                        if(profileModelresponse!.user.level == "35") {nmButton21.isEnabled = true}
+                        if(profileModelresponse!.user.level == "37") {nmButton22.isEnabled = true}
+                        if(profileModelresponse!.user.level == "39") {nmButton23.isEnabled = true}
+                        if(profileModelresponse!.user.level == "40") {nmButton24.isEnabled = true}
+                        if(profileModelresponse!.user.level == "41") {nmButton25.isEnabled = true}
+                        if(profileModelresponse!.user.level == "43") {nmButton26.isEnabled = true}
+                        if(profileModelresponse!.user.level == "45") {nmButton27.isEnabled = true}
+                        if(profileModelresponse!.user.level == "47") {nmButton28.isEnabled = true}
+                        if(profileModelresponse!.user.level == "49") {nmButton29.isEnabled = true}
+                        if(profileModelresponse!.user.level == "50") {nmButton30.isEnabled = true}
+                           
+                      
+                        
+                        
+                    
+                case let .failure(error):
+                    print(error.errorDescription!)
+                    print("hata")
+                }
+            }
+    }
+    
+    
+    
+    
+    
+    @IBAction func btn1(_ sender: Any) {
+        var value = 1
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
         performSegue(withIdentifier: "NightMissionToCase", sender: nil)
     }
-    @IBAction func btnLvl20Click(_ sender: Any) {
-        UserDefaults.standard.set(20, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter5", forKey: "NightHunterName")
-        UserDefaults.standard.set(200, forKey: "NightHunterPow")
+    @IBAction func btn3(_ sender: Any) {
+        var value = 3
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
         performSegue(withIdentifier: "NightMissionToCase", sender: nil)
     }
-    @IBAction func btnLvl25Click(_ sender: Any) {
-        UserDefaults.standard.set(25, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter6", forKey: "NightHunterName")
-        UserDefaults.standard.set(250, forKey: "NightHunterPow")
+    @IBAction func btn5(_ sender: Any) {
+        var value = 5
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
         performSegue(withIdentifier: "NightMissionToCase", sender: nil)
     }
-    @IBAction func btnLvl30Click(_ sender: Any) {
-        UserDefaults.standard.set(30, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter7", forKey: "NightHunterName")
-        UserDefaults.standard.set(300, forKey: "NightHunterPow")
+    @IBAction func btn7(_ sender: Any) {
+        var value = 7
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
         performSegue(withIdentifier: "NightMissionToCase", sender: nil)
     }
-    @IBAction func btnLvl35Click(_ sender: Any) {
-        UserDefaults.standard.set(35, forKey: "NightMissionType")
-        UserDefaults.standard.set("Hunter8", forKey: "NightHunterName")
-        UserDefaults.standard.set(350, forKey: "NightHunterPow")
+    @IBAction func btn9(_ sender: Any) {
+        var value = 9
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn10(_ sender: Any) {
+        var value = 10
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn11(_ sender: Any) {
+        var value = 11
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn13(_ sender: Any) {
+        var value = 13
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn15(_ sender: Any) {
+        var value = 15
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn17(_ sender: Any) {
+        var value = 17
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn19(_ sender: Any) {
+        var value = 19
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn20(_ sender: Any) {
+        var value = 20
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn21(_ sender: Any) {
+        var value = 21
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn23(_ sender: Any) {
+        var value = 23
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn25(_ sender: Any) {
+        var value = 25
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn27(_ sender: Any) {
+        var value = 27
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn29(_ sender: Any) {
+        var value = 29
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn30(_ sender: Any) {
+        var value = 30
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn31(_ sender: Any) {
+        var value = 31
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn33(_ sender: Any) {
+        var value = 33
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn35(_ sender: Any) {
+        var value = 35
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn37(_ sender: Any) {
+        var value = 37
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn39(_ sender: Any) {
+        var value = 39
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn40(_ sender: Any) {
+        var value = 40
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn41(_ sender: Any) {
+        var value = 41
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn43(_ sender: Any) {
+        var value = 43
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn45(_ sender: Any) {
+        var value = 45
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn47(_ sender: Any) {
+        var value = 47
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn49(_ sender: Any) {
+        var value = 49
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
+        performSegue(withIdentifier: "NightMissionToCase", sender: nil)
+    }
+    @IBAction func btn50(_ sender: Any) {
+        var value = 50
+        UserDefaults.standard.set(value, forKey: "NightMissionType")
         performSegue(withIdentifier: "NightMissionToCase", sender: nil)
     }
     
+   
+  
 
 }
