23fcffa94064a7b39d07094cd09663c14ba1c31c
ynsgndz <ynsgndz@hotmail.com.tr>
2023-05-29 00:14:33 +0300

    marketten rastgele  cıkan ıtemi , kuşandıgın ile kıyaslama eklendi. Kuşanma butonunun ayarlanması bekleniyor

--- wildbite/wildbite/Controller/SHOP/SHOP ITEMS/ShopItemsViewController.swift
+++ wildbite/wildbite/Controller/SHOP/SHOP ITEMS/ShopItemsViewController.swift
@@ -12,14 +12,22 @@ class ShopItemsViewController: UIViewController {
 
     var itemResimleri = [String]()
     var itemAdi = [String]()
+    var itemID = [Int]()
+    var itemDressLevel = [String]()
+    var itemLevel = [String]()
+    var itemPower = [String]()
+    var itemDefense = [String]()
+    var itemSpeed = [String]()
+    var itemHealth = [String]()
     @IBOutlet weak var kasaName: UILabel!
+    @IBOutlet weak var userGoldLabel: UILabel!
    
     
     @IBOutlet weak var kasaImage: UIImageView!
     
     @IBOutlet weak var kasaAcButtonLabel: UIButton!
-    @IBOutlet weak var itemGitButtonLabel: UIButton!
-    var countdownSeconds = 3
+    @IBOutlet weak var itemGiyButtonLabel: UIButton!
+    var countdownSeconds = 12
        var timer: Timer?
     
     override func viewDidLoad() {
@@ -34,14 +42,64 @@ class ShopItemsViewController: UIViewController {
         
         print("Shop id: \(UserDefaults.standard.integer(forKey: "ShopMenuID"))")
         print("Shop Name: \(UserDefaults.standard.string(forKey: "ShopMenuAD")!)")
-        print("Shop image: \(UserDefaults.standard.string(forKey: "ShopMenuImage")!)")
-        veriCek()
        
+        print("Shop image: \(UserDefaults.standard.string(forKey: "ShopMenuImage")!)")
+        loadUserProfile()
+        veriCekShop()
+       
+    }
+    fileprivate func loadUserProfile() {
+        let myUserID = UserDefaults.standard.value(forKey: "userID")
+        let myUserName = UserDefaults.standard.value(forKey: "userName")
+        let myUserToken = UserDefaults.standard.value(forKey: "userToken")
+        print("User Id: \(myUserID!)")
+        print("User Name: \(myUserName!)")
+        print("User Token: \(myUserToken!)")
+        
+        let token = "\(myUserToken!)"
+        
+        let headers: HTTPHeaders = [
+            
+            .authorization(bearerToken: token),
+            .accept("application/json")
+            
+        ]
+
+        AF.request("http://yunusgunduz.site/wildbite/public/api/user/\(myUserID!))" , headers: headers )
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseData { [self] response in
+                
+                
+                switch response.result {
+                    case .success:
+                        print("Validation Successful")
+                        
+                        let profileModelresponse = try? JSONDecoder().decode(ProfileModel.self, from: response.data!)
+                        debugPrint(profileModelresponse!)
+                        
+                        print("Debug: Race settings\(profileModelresponse!.race.raceName)")
+                        print("Race: \(profileModelresponse!.race.raceName)")
+                        var itemResimAdi = UserDefaults.standard.array(forKey: "itemCekilisiItemResimleri")
+                        if(  Int((profileModelresponse!.user.gold))! >= 1000) {
+                            kasaAcButtonLabel.isEnabled = true
+                            userGoldLabel.text = "User Gold: \(Int((profileModelresponse!.user.gold))!)"
+                            
+                        }else{
+                            kasaAcButtonLabel.isEnabled = false
+                            userGoldLabel.text = "User Gold: \(Int((profileModelresponse!.user.gold))!)"
+                        }
+                        
+                    case let .failure(error):
+                        print(error.errorDescription!)
+                        print("hata")
+                }
+            }
     }
     // shop id 13 kasalar
     // 1:gold 2 : diamond
 
-  func veriCek() {
+  func veriCekShop() {
  
         let shopID = UserDefaults.standard.integer(forKey: "ShopMenuID")
         let shopAD = UserDefaults.standard.string(forKey: "ShopMenuAD")!
@@ -78,21 +136,31 @@ class ShopItemsViewController: UIViewController {
                             
                             itemResimleri.append(item.image)
                             itemAdi.append(item.name)
+                            itemDressLevel.append(item.dressMinLevel)
+                            itemLevel.append(item.level)
+                            itemPower.append(item.power)
+                            itemSpeed.append(item.speed)
+                            itemDefense.append(item.defense)
+                            itemHealth.append(item.health)
+                            itemID.append(item.id)
                             
                         })
                         
                         UserDefaults.standard.set(itemResimleri, forKey: "itemCekilisiItemResimleri")
                         UserDefaults.standard.set(itemAdi, forKey: "itemCekilisiItemAdi")
+                        UserDefaults.standard.set(itemDressLevel, forKey: "itemCekilisiItemDressLevel")
+                        UserDefaults.standard.set(itemLevel, forKey: "itemCekilisiItemLevel")
+                        UserDefaults.standard.set(itemPower, forKey: "itemCekilisiItemPower")
+                        UserDefaults.standard.set(itemDefense, forKey: "itemCekilisiItemDefense")
+                        UserDefaults.standard.set(itemSpeed, forKey: "itemCekilisiItemSpeed")
+                        UserDefaults.standard.set(itemHealth, forKey: "itemCekilisiItemHealth")
+                        UserDefaults.standard.set(itemID, forKey: "itemCekilisiItemID")
+                        
+                    
                        
                         print("apiden item adedi \(itemResimleri.count)")
                         var itemResimAdi = UserDefaults.standard.array(forKey: "itemCekilisiItemResimleri")
-                        if(itemResimAdi?.count != nil && itemResimAdi?.count != 0) {
-                            kasaAcButtonLabel.isEnabled = true
-                            
-                        }else{
-                            kasaAcButtonLabel.isEnabled = false
-                            
-                        }
+                        
                       
                     case let .failure(error):
                         print(error.errorDescription!)
@@ -112,10 +180,21 @@ class ShopItemsViewController: UIViewController {
    
   
     @objc func updateCountdown() {
-        itemGitButtonLabel.isEnabled = false
+         itemGiyButtonLabel.isEnabled = false
         kasaAcButtonLabel.isEnabled = false
         let itemResimAdiUD = UserDefaults.standard.array(forKey: "itemCekilisiItemResimleri")
         let itemItemAdiUD = UserDefaults.standard.array(forKey: "itemCekilisiItemAdi")
+        
+   
+        
+        let itemItemDressLevelUD = UserDefaults.standard.array(forKey: "itemCekilisiItemDressLevel")
+        let itemItemLevelUD = UserDefaults.standard.array(forKey: "itemCekilisiItemLevel")
+        let itemItemPowerUD = UserDefaults.standard.array(forKey: "itemCekilisiItemPower")
+        let itemItemDefenseUD = UserDefaults.standard.array(forKey: "itemCekilisiItemDefense")
+        let itemItemSpeedUD = UserDefaults.standard.array(forKey: "itemCekilisiItemSpeed")
+        let itemItemHealthUD = UserDefaults.standard.array(forKey: "itemCekilisiItemHealth")
+        let itemItemRandomNumberUD = UserDefaults.standard.array(forKey: "itemCekilisiRandomNumber")
+        
         
         dump(itemResimAdiUD)
         if countdownSeconds > 0 {
@@ -136,28 +215,74 @@ class ShopItemsViewController: UIViewController {
            
             
         dump("\(countdownSeconds)")
-        } else {
+        } else { //MARK: cikan item
             timer?.invalidate()
             if(itemResimAdiUD!.count != 0){
                 let randomNumber = Int.random(in: 0 ... itemResimAdiUD!.count-1)
+                UserDefaults.standard.set(randomNumber, forKey: "itemCekilisiRandomNumber")
                 print("random Number : \(randomNumber)")
                 let urlImage = URL(string: "\(itemResimAdiUD![randomNumber])")
                 DispatchQueue.main.async{ [self] in
                     
                       self.kasaImage.kf.setImage(with: urlImage)
-              
+                    kasaName.text = "\(itemItemAdiUD![randomNumber])"
                       
                   }
-                kasaName.text = "\(itemItemAdiUD![randomNumber])"
+               
             }
-            itemGitButtonLabel.isEnabled = true
-            kasaAcButtonLabel.isEnabled = true
+             itemGiyButtonLabel.isEnabled = true
+            kasaAcButtonLabel.isEnabled = false
             kasaAcButtonLabel.setTitle("yeniden aç", for: .normal)
-            countdownSeconds = 3
+            countdownSeconds = 12
+            loadUserProfile()
         }
     }
     @IBAction func kasaAc(_ sender: Any) {
  
         startCountdown()
+        userStateArttir(pow: 0, def: 0, spd: 0, totlDamge: 0, gld: -1000)
+    }
+    
+    func userStateArttir(pow:Int, def:Int, spd:Int, totlDamge:Int, gld:Int){
+        let myUserID = UserDefaults.standard.value(forKey: "userID")
+        let myUserName = UserDefaults.standard.value(forKey: "userName")
+        let myUserToken = UserDefaults.standard.value(forKey: "userToken")
+        print("User Id: \(myUserID!)")
+        print("User Name: \(myUserName!)")
+        print("User Token: \(myUserToken!)")
+        
+                let token = "\(myUserToken!)"
+        let headers: HTTPHeaders = [
+            .authorization(bearerToken: token),
+            .accept("application/json")
+        ]
+        let userStateParameters = UserStateUpdate(power: pow, defense: def, speed: spd, total_damage: totlDamge, gold: gld)
+        
+      AF.request("http://yunusgunduz.site/wildbite/public/api/user-statu/\(myUserID!)",
+                 method: .put,
+                 parameters: userStateParameters,
+                 headers: headers)
+      .validate(statusCode: 200..<500)
+      .validate(contentType: ["application/json"])
+      .responseData { [self]  response in
+          debugPrint(response)
+          switch response.result {
+          case .success:
+                  let userStateUpdateResponse = try? JSONDecoder().decode(UserStateModel.self,  from: response.data!)
+                  debugPrint(userStateUpdateResponse ?? "userStateUpdateResponse ariza")
+                  dump("User state message: \(userStateUpdateResponse!.the1)")
+                  dump("User state power: \(userStateUpdateResponse!.user.power)")
+                  dump("User state def: \(userStateUpdateResponse!.user.defense)")
+                  dump("User state speed: \(userStateUpdateResponse!.user.speed)")
+                  dump("User state total damage: \(userStateUpdateResponse!.user.totalDamage)")
+                  dump("User state gold: \(userStateUpdateResponse!.user.gold)")
+                
+                  viewWillAppear(true)
+          case let .failure(error):
+              print(error.errorDescription!)
+              print("hata")
+          }
+      }
+
     }
 }
---
+++ wildbite/wildbite/Controller/SHOP/SHOPITEMDETAILS/ShopItemDetailsViewController.swift
@@ -0,0 +1,231 @@
+//
+//  ShopItemDetailsViewController.swift
+//  wildbite
+//
+//  Created by Yunus Gündüz on 28.05.2023.
+//
+
+import UIKit
+import Alamofire
+import Kingfisher
+
+class ShopItemDetailsViewController: UIViewController {
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemLevelLabel: UILabel!
+    @IBOutlet weak var itemDressLevelLabel: UILabel!
+    @IBOutlet weak var itemPowerLabel: UILabel!
+    @IBOutlet weak var itemDefenseLabel: UILabel!
+    @IBOutlet weak var itemSpeedLabel: UILabel!
+    @IBOutlet weak var itemHealthLabel: UILabel!
+    
+    @IBOutlet weak var kasaItemNameLabel: UILabel!
+    @IBOutlet weak var kasaItemLevelLabel: UILabel!
+    @IBOutlet weak var kasaItemDressLevelLabel: UILabel!
+    @IBOutlet weak var kasaItemPowerLabel: UILabel!
+    @IBOutlet weak var kasaItemDefenseLabel: UILabel!
+    @IBOutlet weak var kasaItemSpeedLabel: UILabel!
+    @IBOutlet weak var kasaItemHealthLabel: UILabel!
+    
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var kasaItemImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        let itemResimAdiUD = UserDefaults.standard.array(forKey: "itemCekilisiItemResimleri")
+        let itemItemAdiUD = UserDefaults.standard.array(forKey: "itemCekilisiItemAdi")
+        let itemItemID = UserDefaults.standard.array(forKey: "itemCekilisiItemID")
+        
+        let itemItemDressLevelUD = UserDefaults.standard.array(forKey: "itemCekilisiItemDressLevel")
+        let itemItemLevelUD = UserDefaults.standard.array(forKey: "itemCekilisiItemLevel")
+        let itemItemPowerUD = UserDefaults.standard.array(forKey: "itemCekilisiItemPower")
+        let itemItemDefenseUD = UserDefaults.standard.array(forKey: "itemCekilisiItemDefense")
+        let itemItemSpeedUD = UserDefaults.standard.array(forKey: "itemCekilisiItemSpeed")
+        let itemItemHealthUD = UserDefaults.standard.array(forKey: "itemCekilisiItemHealth")
+        let itemItemRandomNumberUD = UserDefaults.standard.integer(forKey: "itemCekilisiRandomNumber")
+        
+        let url = URL(string: itemResimAdiUD![itemItemRandomNumberUD] as! String)!
+        kasaItemImageView.kf.setImage(with: url )
+        kasaItemDressLevelLabel.text = (itemItemDressLevelUD![itemItemRandomNumberUD] as! String)
+        kasaItemNameLabel.text = (itemItemAdiUD![itemItemRandomNumberUD] as! String)
+        kasaItemLevelLabel.text = "+\((itemItemLevelUD![itemItemRandomNumberUD] as! String))"
+        kasaItemPowerLabel.text = (itemItemPowerUD![itemItemRandomNumberUD] as! String)
+        kasaItemDefenseLabel.text = (itemItemDefenseUD![itemItemRandomNumberUD] as! String)
+        kasaItemSpeedLabel.text = (itemItemSpeedUD![itemItemRandomNumberUD] as! String)
+        kasaItemHealthLabel.text = (itemItemHealthUD![itemItemRandomNumberUD] as! String)
+        loadUserDressedItems()
+    }
+
+    @IBAction func itemKusan(_ sender: Any) {
+        
+        
+        
+        
+    }
+    fileprivate func loadUserDressedItems() {
+        let myUserID = UserDefaults.standard.value(forKey: "userID")
+        let myUserName = UserDefaults.standard.value(forKey: "userName")
+        let myUserToken = UserDefaults.standard.value(forKey: "userToken")
+        print("User Id: \(myUserID!)")
+        print("User Name: \(myUserName!)")
+        print("User Token: \(myUserToken!)")
+        
+        let token = "\(myUserToken!)"
+        
+        let headers: HTTPHeaders = [
+            
+            .authorization(bearerToken: token),
+            .accept("application/json")
+            
+        ]
+
+        AF.request("http://yunusgunduz.site/wildbite/public/api/dressed-enemy/\(myUserID!))" , headers: headers )
+            .validate(statusCode: 200..<500)
+            .validate(contentType: ["application/json"])
+            .responseData { [self] response in
+                
+                
+                switch response.result {
+                    case .success:
+                        print("dress list Successful")
+                        print("Shop Name: \(UserDefaults.standard.string(forKey: "ShopMenuAD")!)")
+                        let tiklananItem = UserDefaults.standard.string(forKey: "ShopMenuAD")!
+                        let dressResponse = try? JSONDecoder().decode(UserDressedModel.self, from: response.data!)
+                        debugPrint(dressResponse ?? "dress list bisey oldu")
+                       
+                        dump("Item + user pow\(dressResponse!.userPower)")
+                        dump("Item + user health\(dressResponse!.userCurrentHealth)")
+                        dump("Item + user def\(dressResponse!.userDefense)")
+                        dump("Item + user speed\(dressResponse!.userSpeed)")
+                       
+                        dump("Only Item pow: \(dressResponse!.itemPower)")
+                        dump("Only Item health: \(dressResponse!.itemHealth)")
+                        dump("Only Item def: \(dressResponse!.itemDefense)")
+                        dump("Only Item speed: \(dressResponse!.itemSpeed)")
+                        
+                        dressResponse?.dressed.forEach({ itemler in
+                            switch itemler.shopName{
+                                case "kask":
+                                    if(tiklananItem == "kask"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                   
+                                case "zırh":
+                                    if(tiklananItem == "zırh"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                  
+                                case "silah":
+                                    if(tiklananItem == "silah"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                   
+                                case "kalkan":
+                                    if(tiklananItem == "kalkan"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                   
+                                case "kolye":
+                                    if(tiklananItem == "kolye"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                   
+                                case "eldiven":
+                                    if(tiklananItem == "eldiven"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                  
+                                case "bot":
+                                    if(tiklananItem == "ayakkabi"){
+                                        let url = URL(string: itemler.image)!
+                                        itemImageView.kf.setImage(with: url )
+                                        itemNameLabel.text = "\(itemler.name)"
+                                        itemLevelLabel.text = "+\(itemler.level)"
+                                        
+                                        itemDressLevelLabel.text = " \(itemler.dressMinLevel)"
+                                        itemPowerLabel.text = "\(itemler.power)"
+                                        itemDefenseLabel.text = "\(itemler.defense)"
+                                        itemSpeedLabel.text = "\(itemler.speed)"
+                                        itemHealthLabel.text = "\(itemler.health)"
+                                    }
+                                    
+                                    
+                                
+                                default:
+                                    break
+                            }
+                            
+                           
+                        })
+                       
+                        
+                        
+                        
+                       
+                        
+                    case let .failure(error):
+                        print(error.errorDescription!)
+                        print("hata")
+                }
+            }
+    }
+
+}
